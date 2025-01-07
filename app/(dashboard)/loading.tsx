dfc9acfc2068ac91c75168c85a2f00852a45089f
remus-wolico <135813551+remus00@users.noreply.github.com>
2025-01-07 15:03:33 +0100

    initial ui for dashboard page

---
+++ app/(dashboard)/loading.tsx
@@ -0,0 +1,5 @@
+const HomePageLoading = () => {
+    return null;
+};
+
+export default HomePageLoading;
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,51 @@
-const HomePage = () => {
-    return <div>HomePage</div>;
+import { CreateTransactionDialog } from '@/components/custom/create-transaction-dialog';
+import { Button } from '@/components/ui/button';
+import { db } from '@/lib/prisma';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
+const HomePage = async () => {
+    const user = await currentUser();
+
+    if (!user) redirect('/sign-in');
+
+    const userSetting = await db.userSettings.findUnique({
+        where: {
+            userId: user.id,
+        },
+    });
+
+    if (!userSetting) redirect('/wizard');
+
+    return (
+        <div className="h-full bg-background">
+            <div className="border-b bg-card">
+                <div className="container mx-auto flex flex-wrap items-center justify-between gap-6 px-8 py-8">
+                    <p className="text-3xl font-bold">Hello, {user.firstName}! 👋🏻</p>
+
+                    <div className="flex items-center gap-3">
+                        <CreateTransactionDialog
+                            type="income"
+                            trigger={
+                                <Button variant="outline" className="green-btn">
+                                    New income 🤑
+                                </Button>
+                            }
+                        />
+
+                        <CreateTransactionDialog
+                            type="expense"
+                            trigger={
+                                <Button variant="outline" className="rose-btn">
+                                    New expense 🫡
+                                </Button>
+                            }
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default HomePage;
