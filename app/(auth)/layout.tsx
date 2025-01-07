2520dd44ab28eaddbb9710971d6fa499af52018c
remus-wolico <135813551+remus00@users.noreply.github.com>
2025-01-07 11:08:05 +0100

    add authentication with clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import { Logo } from '@/components/logo';
+import { ReactNode } from 'react';
+
+const AuthLayout = ({ children }: { children: ReactNode }) => {
+    return (
+        <div className="relative flex h-screen w-full flex-col items-center justify-center">
+            <Logo />
+            <div className="mt-12">{children}</div>
+        </div>
+    );
+};
+
+export default AuthLayout;
