a23a047c3b550137056cae8a04ada765c312ab3d
remus-wolico <135813551+remus00@users.noreply.github.com>
2025-01-07 12:55:24 +0100

    install tanstack query

--- components/providers/root-providers.tsx
+++ components/providers/root-providers.tsx
@@ -1,16 +1,23 @@
 'use client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'next-themes';
 import { PropsWithChildren } from 'react';
 
 export const RootProviders = ({ children }: PropsWithChildren) => {
+    const queryClient = new QueryClient();
+
     return (
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-        >
-            {children}
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="dark"
+                enableSystem
+                disableTransitionOnChange
+            >
+                {children}
+            </ThemeProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     );
 };
